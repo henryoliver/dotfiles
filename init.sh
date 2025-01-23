2c62adb9fef02af75742feaa0e5499c4c1365624
Henry Oliver <henry_oliver@me.com>
2025-01-23 13:11:09 -0500

    config: add new ghostty config

--- init.sh
+++ init.sh
@@ -9,6 +9,7 @@ mkdir ~/.config/nvim
 
 ln -sv ~/.dotfiles/lsp ~/.config/lsp
 ln -sv ~/.dotfiles/kitty ~/.config/kitty
+ln -sv ~/.dotfiles/ghostty ~/.config/ghostty 
 
 ln -sv ~/.dotfiles/nvim/init.lua ~/.config/nvim
 ln -sv ~/.dotfiles/nvim/lua ~/.config/nvim
