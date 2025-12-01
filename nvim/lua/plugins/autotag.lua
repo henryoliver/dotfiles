ea7f3843ca5b274631b5cf289becc74ac6d8d218
Henry Oliver <henry_oliver@me.com>
2025-12-01 16:49:23 -0500

    chore: update gitignore and refactor autotag configuration

--- nvim/lua/plugins/autotag.lua
+++ nvim/lua/plugins/autotag.lua
@@ -1,9 +1,15 @@
 return {
     "windwp/nvim-ts-autotag",
     event = "InsertEnter",
-    opts = {
-        enable_close = true, -- Auto close tags
-        enable_rename = true, -- Auto rename pairs of tags
-        enable_close_on_slash = false, -- Auto close on trailing </
-    },
+    config = function()
+        local autotag = require("nvim-ts-autotag")
+
+        autotag.setup({
+            opts = {
+                enable_close = true, -- Auto close tags
+                enable_rename = true, -- Auto rename pairs of tags
+                enable_close_on_slash = false, -- Auto close on trailing </
+            },
+        })
+    end,
 }
